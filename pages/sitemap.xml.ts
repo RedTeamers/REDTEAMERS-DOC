af4e7c91dae3a6a8ab88899e185499bd04176292
0xr <ghilesmahleb@gmail.com>
2025-08-16 01:46:50 +0100

    good night

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -2,8 +2,7 @@
 export const config = { runtime: 'experimental-edge' }
 
 const urls = [
-  '/', '/RTW', '/about',
-  '/news/2025/08/lancement-plateforme', '/authors/riles'
+  '/', '/RTW', '/about'
 ]
 
 export default async function handler() {
